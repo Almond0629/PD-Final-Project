5c1674baf9a373f95019b98be3e20c8f178d6220
qyWu2024 <qywu2024@gmail.com>
2024-12-09 11:47:00 +0800

    Update fight.h

--- fight.h
+++ fight.h
@@ -7,13 +7,14 @@ bool Character::playOrPay(){
     while (backpack.money >= 10) {
         cout << "要對戰還是要賄賂怪物？ (1: 接受挑戰，2: 付10元賄賂怪物)" << endl;
         cout << "請輸入選擇：";
-        int startChoice;
-        cin >> startChoice;
-
-        if (startChoice == 1) {
+        string startChoice;
+        cin >> std::ws;
+        getline(cin, startChoice);
+        
+        if (startChoice == "1") {
             cout << "你接受挑戰！" << endl;
             return true;
-        } else if (startChoice == 2) {
+        } else if (startChoice == "2") {
             cout << "你選擇賄賂。已從錢包扣10元。Shame on you!" << endl;
             reduceMoneyAndPrint(10);
             return false;
@@ -22,6 +23,7 @@ bool Character::playOrPay(){
             continue;
         }
     }
+    
     if(backpack.money < 10) {
         cout << "你已經沒錢了！接受挑戰吧！" << endl;
         return true;
@@ -31,32 +33,33 @@ bool Character::playOrPay(){
 void Character::fight(Monster& monster){
     cout << "野生的 " << monster.name << " 突然跳出來擋著你！" << endl;
     cout << "他決定以剪刀石頭布來對戰！" << endl;
-    while (life > 0) {
-        if(playOrPay()){ // accept the challenge
+    if(playOrPay()){ // accept the challenge
+        while (life > 0) {
             cout << "輸入 5 (布)，2 (剪刀)，0 (石頭) 來對戰。" << endl;
             cout << "輸入你的選擇： ";
-            int playerChoice;
-            cin >> playerChoice;
-
-            if (playerChoice != 5 && playerChoice != 2 && playerChoice != 0) {
+            string playerChoice;
+            cin >> std::ws;
+            getline(cin, playerChoice);
+    
+            if (playerChoice != "5" && playerChoice != "2" && playerChoice != "0") {
                 cout << "不明的輸入，請輸入 5 (布)，2 (剪刀)，0 (石頭)！" << endl;
                 continue;
             }
-
+    
             int monsterChoice = monster.RPSchoiceAndPrint();
             // player wins
-            if ((playerChoice == 5 && monsterChoice == 0) || 
-                (playerChoice == 2 && monsterChoice == 5) || 
-                (playerChoice == 0 && monsterChoice == 2)) {
+            if ((playerChoice == "5" && monsterChoice == 0) || 
+                (playerChoice == "2" && monsterChoice == 5) || 
+                (playerChoice == "0" && monsterChoice == 2)) {
                 putIntoBackPack(monster);
                 cout << "你獲勝！ 已收服了 " << monster.name << " 進你的背包！" << endl;
                 cout << "繼續冒險下去，並找到更多食材吧！" << endl;
                 break;
             } 
             // player lost
-            else if ((monsterChoice == 5 && playerChoice == 0) || 
-                    (monsterChoice == 2 && playerChoice == 5) || 
-                    (monsterChoice == 0 && playerChoice == 2)) {
+            else if ((monsterChoice == 5 && playerChoice == "0") || 
+                    (monsterChoice == 2 && playerChoice == "5") || 
+                    (monsterChoice == 0 && playerChoice == "2")) {
                 cout << "啊！你輸了！" << endl;
                 life--;
                 if(life == 0) { break; }
@@ -69,12 +72,15 @@ void Character::fight(Monster& monster){
                 cout << "平手！再輸入一次！" << endl;
                 continue;
             }
-        }
-        else {
-            break;
+            
+            if (life == 0) {
+                cout << "你被煮了！" << endl;
+                break;
+            }
         }
     }
     if (life == 0) {
         cout << "你被煮了！" << endl;
     }
+
 }
