ed2b0fab6079c92dffb2498cbc636b64b4ea3b79
qyWu2024 <qywu2024@gmail.com>
2024-12-09 14:54:58 +0800

    Update fight.h

--- fight.h
+++ fight.h
@@ -1,7 +1,85 @@
 #pragma once
 #include <iostream>
+#include <cstdlib> 
+#include <chrono>
+#include <thread>
 #include "characterMonster.h"
 using namespace std;
+
+const int delayTime = 2;
+
+void gameOver(){
+
+    cout << "告別的時間總是來得特別快..." << endl;
+    sleep(delayTime);
+    cout << "離開迷宮之前，我有個忠告要給你" << endl;
+    sleep(delayTime);
+    cout << "今天午餐就吃活大自助餐吧!還有..." << endl;
+    sleep(delayTime);
+    cout << "吃飽之後就快去讀書!" << endl;
+    cout << "為你的GPA奮鬥!" << endl;
+    sleep(delayTime);
+    cout << "Game Over!" << endl;
+    exit(0);
+}
+
+bool Character::restart(){
+    string answer;
+    cout << "這時有個神祕的聲音出現!" << endl;
+    sleep(delayTime);
+    cout << "我是迷宮陰間的掌管者" << endl;
+    sleep(delayTime);
+    cout << "現在你可以選擇是否要復活" << endl;
+    cout << "在險惡的迷宮中，復活的代價就是..." << endl;
+    sleep(delayTime);
+    cout << "一條命10元!如果你沒錢了..." << endl;
+    sleep(delayTime);
+    cout << "就烹調背包中的食材並吃下營養的番茄牛肉燉湯吧!" << endl;
+    cout << "這樣一來你一樣可以恢復一條命" << endl;
+    cout << "想要復活並繼續冒險嗎？(Y/N)" << endl;
+    cin >> std::ws;
+    getline(cin, answer);
+    
+    if (answer == "Y" || answer == "y") {
+        if(backpack.money < 10 && (backpack.beefCnt == 0 || backpack.tomatoCnt == 0)){
+            cout << "你的錢包餘額: " << backpack.money << endl;
+            cout << "錢包餘額不足!" << endl;
+            cout << "你剩下 " << backpack.tomatoCnt << " 顆番茄與" << backpack.beefCnt << " 塊牛肉。" << endl;
+            cout << "食材不足!" << endl;
+            sleep(delayTime);
+            gameOver();
+        }
+        else {
+            cout << "你的錢包餘額: " << backpack.money << endl;
+            if(backpack.money < 10) {
+                cout << "錢包餘額不足!" << endl;
+                cout << "你剩下 " << backpack.tomatoCnt << " 顆番茄與" << backpack.beefCnt << " 塊牛肉。" << endl;
+                cout << "大廚使用了一顆番茄與一塊牛肉做成營養的番茄牛肉燉湯!" << endl;
+                cout << "吃下營養的番茄牛肉燉湯並恢復元氣吧!" << endl;
+                backpack.tomatoCnt--;
+                backpack.beefCnt--;
+                cout << "你剩下 " << backpack.tomatoCnt << " 顆番茄與" << backpack.beefCnt << " 塊牛肉。" << endl;
+                sleep(delayTime);
+            }
+            else{
+                cout << "太棒了!辛苦賺來的錢足以讓你復活!" << endl;
+                sleep(delayTime);
+                backpack.money -= 10;
+                cout << "你的錢包餘額: " << backpack.money << endl;
+            }
+            this->life++;
+            cout << "現在你擁有1條新生命!"<< endl;
+            cout << "回到陽間繼續冒險吧!" << endl;
+            sleep(delayTime);
+        }
+
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
 
 bool Character::playOrPay(){
     while (backpack.money >= 10) {
@@ -75,12 +153,14 @@ void Character::fight(Monster& monster){
             
             if (life == 0) {
                 cout << "你被煮了！" << endl;
-                break;
+                if(this->restart()) continue;
+                else gameOver();
             }
         }
     }
     if (life == 0) {
         cout << "你被煮了！" << endl;
+        if(!this->restart()) gameOver();
     }
 
 }
